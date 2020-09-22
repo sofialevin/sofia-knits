d2b347486a1ead078b5986533512c3e8c569ecd8
Sofia Levin <sofiafontes@me.com>
2020-09-21 23:28:25 -0700

    Updated config file.

--- config/index.js
+++ config/index.js
@@ -4,18 +4,18 @@ module.exports = {
   siteTitle: 'Sofia Knits', // Navigation and Site Title
   siteTitleAlt: "Sofia Knits", // Alternative Site title for SEO
   siteTitleShort: 'Sofia Knits', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
+  siteHeadline: 'If I sit I knit', // Headline for schema.org JSONLD
   siteUrl: 'https://sofiaknits.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
+  siteDescription: 'Sofia\'s knitting projects',
   author: 'Sofia Knits', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@sofiaflevin', // Twitter Username
   ogSiteName: 'sofiaflevin', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
-  googleAnalyticsID: 'UA-XXXXXX-X',
+  googleAnalyticsID: 'UA-170171245-3',
 
   // Manifest and Progress color
   themeColor: '#db7436',
